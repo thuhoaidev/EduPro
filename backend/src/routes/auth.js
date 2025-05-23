2ecca0932375c1cca02f553f46884c95593e2e25
zonglaptrinh <dungdtph38403@fpt.edu.vn>
2025-05-24 00:09:40 +0700

    Revert "danh sách và phê duyệt giảng viên"
    This reverts commit d701b83e5abba91979a6b90c3c35c45b929b3c78.

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -9,7 +9,6 @@ const {
   verifyEmail,
 } = require('../controllers/auth');
 const {updateOrCreateInstructorProfile} = require('../controllers/instructorprofile')
-const {getPendingInstructors, approveInstructorProfile} = require('../controllers/instructorapproval')
 const { auth, checkRole, checkPermission, requireAuth } = require('../middlewares/auth');
 const { ROLES } = require('../constants/roles');
 // const userController = require('../controllers/user');
@@ -27,8 +26,6 @@ router.post('/verify-email', verifyEmail);
 
 // Routes hồ sơ giảng viên
 router.put('/instructor-profile', auth, requireAuth, updateOrCreateInstructorProfile);
-router.get('/instructor-approval', auth, checkRole(ROLES.ADMIN, ROLES.MODERATOR), getPendingInstructors);
-router.post('/instructor-approval', auth, checkRole(ROLES.ADMIN, ROLES.MODERATOR), approveInstructorProfile);
 
 // Route chỉ cho admin
 // router.get('/admin/users', auth, checkRole(ROLES.ADMIN), userController.getAllUsers);
