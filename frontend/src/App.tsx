e03ea5de47bf9391617f7d1d7b3a753711b68dac
Phạm Đức Doanh <doanhpdph48812@gmail.com>
2025-05-28 08:43:42 +0700

    Cập nhật thông tin người dùng

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import SigninPage from "./pages/auth/login";
-import SignupPage from "./pages/auth/register";
+
+import UserProfilePage from "./pages/profile";
 
 function App() {
   return (
     <>
 
       <Routes>
-        <Route path="login" element={<SigninPage />} />
-        <Route path="register" element={<SignupPage />} />
+        {/* <Route path="login" element={<SigninPage />} />
+        <Route path="register" element={<SignupPage />} /> */}
+        <Route path="/profile" element={<UserProfilePage />} />
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </>
