9b323edf859526357abd9ea98ef36d9d729df29b
thuhoai13 <thuhoaiishere@gmail.com>
2025-05-28 09:40:40 +0700

    Revert "Cập nhật thông tin người dùng"
    This reverts commit e03ea5de47bf9391617f7d1d7b3a753711b68dac.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-
-import UserProfilePage from "./pages/profile";
+import SigninPage from "./pages/auth/login";
+import SignupPage from "./pages/auth/register";
 
 function App() {
   return (
     <>
 
       <Routes>
-        {/* <Route path="login" element={<SigninPage />} />
-        <Route path="register" element={<SignupPage />} /> */}
-        <Route path="/profile" element={<UserProfilePage />} />
+        <Route path="login" element={<SigninPage />} />
+        <Route path="register" element={<SignupPage />} />
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </>
