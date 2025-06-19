2c9e03b2692e7d5e0343d5fba696a9135c9df550
thuhoai13 <thuhoaiishere@gmail.com>
2025-06-19 18:56:12 +0700

    lấy danh sách khóa học
    lấy danh sách khóa học

--- backend/src/routes/course.routes.js
+++ backend/src/routes/course.routes.js
@@ -5,18 +5,18 @@ const { uploadAvatar, handleUploadError } = require('../middlewares/upload.middl
 const { auth, requireAuth } = require('../middlewares/auth');
 
 // Routes
-router.post('/', auth, requireAuth(['instructor', 'admin']), uploadAvatar, handleUploadError, courseController.createCourse);
-router.put('/:id', auth, requireAuth, uploadAvatar, handleUploadError, courseController.updateCourse);
-router.patch('/:id/status', auth, requireAuth, courseController.updateCourseStatus);
-router.delete('/:id', auth, requireAuth(['instructor', 'admin']), courseController.deleteCourse);
+router.post('/', auth, requireAuth(['instructor']), uploadAvatar, handleUploadError, courseController.createCourse);
+router.put('/:id', auth, requireAuth(['instructor']), uploadAvatar, handleUploadError, courseController.updateCourse);
+router.patch('/:id/status', auth, requireAuth(['instructor']), courseController.updateCourseStatus);
+router.delete('/:id', auth, requireAuth(['instructor']), courseController.deleteCourse);
 // Lấy danh sách khóa học
-router.get('/', auth, requireAuth(['instructor', 'admin']), courseController.getCourses);
+router.get('/', courseController.getCourses);
 
 // Lấy chi tiết khóa học
-router.get('/:id', auth, requireAuth(['admin', 'instructor']), courseController.getCourseById);
-router.get('/slug/:slug', auth, requireAuth(['admin', 'instructor']), courseController.getCourseBySlug);
+router.get('/:id', courseController.getCourseById);
+router.get('/slug/:slug', courseController.getCourseBySlug);
 
 // Lấy danh sách chương học và bài học theo khóa học
-router.get('/:course_id/sections', auth, requireAuth(['admin', 'instructor']), courseController.getCourseSectionsAndLessons);
+router.get('/:course_id/sections', courseController.getCourseSectionsAndLessons);
 
 module.exports = router; 
